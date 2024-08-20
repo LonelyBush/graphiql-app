ba05a801ddc2c7e86b3a6726fea62c8f1dedbd27
LonelyBush <lonely.bush68@gmail.com>
2024-08-20 14:37:43 +0300

    reafactor: changed main derictory folder name

---
+++ graphiql-app/vite.config.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), remix({
+    appDirectory: 'src/app',
+  })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+    },
+    setupFiles: ['./src/vitest-setup/test.ts'],
+  },
+});
