ca116e0ca6de1a9570341ee967028b5e3caf9f86
LonelyBush <lonely.bush68@gmail.com>
2024-08-21 15:08:50 +0300

    fix: fixed vite.config.ts deleted unnecessary react() plugin, remix() has all react staf

--- graphiql-app/vite.config.ts
+++ graphiql-app/vite.config.ts
@@ -3,12 +3,10 @@
 /// <reference types="vite/client" />
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     remix({
       appDirectory: 'src/app',
     }),
