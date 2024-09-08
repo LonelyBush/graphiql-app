67e6698770e8e3c540c103e3afa2576f5737c458
Yana <annetart1994@gmail.com>
2024-09-08 13:58:54 +0300

    feat: add test to errorboundary

--- graphiql-app/src/components/component/error-boundary/error-boundary.test.tsx
+++ graphiql-app/src/components/component/error-boundary/error-boundary.test.tsx
@@ -1,34 +1,36 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; 
+import { MemoryRouter } from 'react-router-dom';
 import ErrorBoundaryComponent from './error-boundary';
 
 const MockComponent = () => {
-  throw new Error('Test error'); 
+  throw new Error('Test error');
 };
 
 describe('ErrorBoundary', () => {
   it('renders the fallback UI when there is an error', () => {
     render(
-      <MemoryRouter> 
+      <MemoryRouter>
         <ErrorBoundaryComponent>
           <MockComponent />
         </ErrorBoundaryComponent>
-      </MemoryRouter>
+      </MemoryRouter>,
     );
 
     expect(screen.getByText('ErrorMessage')).toBeInTheDocument();
     expect(screen.getByText('BackToMain')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'BackToMain' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'BackToMain' }),
+    ).toBeInTheDocument();
   });
 
   it('renders the children when there is no error', () => {
     render(
-      <MemoryRouter> 
+      <MemoryRouter>
         <ErrorBoundaryComponent>
           <div>Child Component</div>
         </ErrorBoundaryComponent>
-      </MemoryRouter>
+      </MemoryRouter>,
     );
 
     expect(screen.getByText('Child Component')).toBeInTheDocument();
