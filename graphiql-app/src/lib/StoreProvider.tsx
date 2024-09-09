d1bb31c36c6b81bba2a304824a0d42323552f683
Yana <annetart1994@gmail.com>
2024-09-09 19:28:34 +0300

    feat: add test to store components

--- graphiql-app/src/lib/StoreProvider.tsx
+++ graphiql-app/src/lib/StoreProvider.tsx
@@ -2,16 +2,23 @@ import React, { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, AppStore } from './store';
 
-export default function StoreProvider({
-  children,
-}: {
+interface StoreProviderProps {
   children: React.ReactNode;
-}) {
+  store?: AppStore;
+}
+
+function StoreProvider({ children, store: externalStore }: StoreProviderProps) {
   const storeRef = useRef<AppStore>();
 
   if (!storeRef.current) {
-    storeRef.current = makeStore();
+    storeRef.current = externalStore || makeStore();
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
+
+StoreProvider.defaultProps = {
+  store: undefined,
+};
+
+export default StoreProvider;
