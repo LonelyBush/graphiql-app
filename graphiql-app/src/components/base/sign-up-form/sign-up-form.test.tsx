cfeb4e4ef2e16581d2e5dc9a27445930ab0a02f8
LonelyBush <lonely.bush68@gmail.com>
2024-09-12 22:19:24 +0300

    feat: added sign-up-form.tests

--- graphiql-app/src/components/base/sign-up-form/sign-up-form.test.tsx
+++ graphiql-app/src/components/base/sign-up-form/sign-up-form.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
 import { useNavigate } from '@remix-run/react';
 import { ToastContainer } from 'react-toastify';
 import userEvent from '@testing-library/user-event';
 import { ReactNode } from 'react';
-import LoginForm from './login-form';
 import useAuth from '../../../utils/hooks/useAuth-hook';
-import { logInWithEmailAndPassword } from '../../../utils/firebase-auth/firebase';
+import SignUpForm from './sign-up-form';
+import { registerWithEmailAndPassword } from '../../../utils/firebase-auth/firebase';
 
 const renderWithToastify = (component: ReactNode) => {
   return render(
@@ -18,92 +18,96 @@ const renderWithToastify = (component: ReactNode) => {
 };
 vi.mock('../../../utils/hooks/useAuth-hook');
 vi.mock('../../../utils/firebase-auth/firebase', () => ({
-  logInWithEmailAndPassword: vi.fn(),
+  registerWithEmailAndPassword: vi.fn(),
 }));
 vi.mock('@remix-run/react', () => ({
   useNavigate: vi.fn(),
 }));
-vi.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
-}));
 
-describe('LoginForm Component', () => {
+describe('Sign up form component', () => {
   const mockNavigate = vi.fn();
-
   beforeEach(() => {
     vi.resetAllMocks();
     (useNavigate as Mock).mockReturnValue(mockNavigate);
     (useAuth as Mock).mockReturnValue({ user: null, loading: false });
   });
   it('renders form fields and submit button', () => {
-    const { getByText, getByRole } = render(<LoginForm />);
+    const { getByText, getByRole } = render(<SignUpForm />);
+    expect(getByText('Nickname')).toBeInTheDocument();
     expect(getByText('Email')).toBeInTheDocument();
     expect(getByText('Password')).toBeInTheDocument();
+    expect(getByText('ConfirmPassword')).toBeInTheDocument();
     expect(getByRole('button', { name: 'Submit' })).toBeInTheDocument();
   });
 
   it('renders forms error on submit empty form', async () => {
-    const { getByRole } = render(<LoginForm />);
+    const { getByRole } = render(<SignUpForm />);
     userEvent.click(getByRole('button', { name: 'Submit' }));
     await waitFor(() => {
       expect(screen.getByText('EmailRequired')).toBeInTheDocument();
+      expect(screen.getByText('NicknameRequired')).toBeInTheDocument();
       expect(screen.getByText('PasswordRequired')).toBeInTheDocument();
+      expect(screen.getByText('ConfirmPassword')).toBeInTheDocument();
     });
   });
+
   it('displays error toast if login fails', async () => {
-    (logInWithEmailAndPassword as Mock).mockRejectedValueOnce(
+    (registerWithEmailAndPassword as Mock).mockRejectedValueOnce(
       new Error('authError'),
     );
 
     const { getByRole, getByPlaceholderText, getByText } = renderWithToastify(
-      <LoginForm />,
+      <SignUpForm />,
     );
-
+    await userEvent.type(getByPlaceholderText('JohnDoe'), 'JohnDoe');
     await userEvent.type(
       getByPlaceholderText('example@gmail.com'),
       'barbara4@mail.com',
     );
-    await userEvent.type(getByPlaceholderText(''), '!Qwer2333');
-    fireEvent.click(getByRole('button', { name: 'Submit' }));
+    await userEvent.type(getByPlaceholderText('Password'), '!Qwer2333');
+    await userEvent.type(getByPlaceholderText('ConfirmPassword'), '!Qwer2333');
+    userEvent.click(getByRole('button', { name: 'Submit' }));
     await waitFor(() => {
       expect(getByText('unexpectedError')).toBeInTheDocument();
     });
   });
 
-  it('displays loading toast when pending', async () => {
-    (logInWithEmailAndPassword as Mock).mockResolvedValueOnce({
-      user: { email: 'barbara4@mail.com' },
+  it('displays succesfull message when success sign up ', async () => {
+    (registerWithEmailAndPassword as Mock).mockResolvedValueOnce({
+      user: { email: 'barbara4@mail.com', nickname: 'JohnDoe' },
     });
 
     const { getByRole, getByPlaceholderText, getByText } = renderWithToastify(
-      <LoginForm />,
+      <SignUpForm />,
     );
-
+    await userEvent.type(getByPlaceholderText('JohnDoe'), 'JohnDoe');
     await userEvent.type(
       getByPlaceholderText('example@gmail.com'),
       'barbara4@mail.com',
     );
-    await userEvent.type(getByPlaceholderText(''), '!Qwer23');
-    fireEvent.click(getByRole('button', { name: 'Submit' }));
+    await userEvent.type(getByPlaceholderText('Password'), '!Qwer2333');
+    await userEvent.type(getByPlaceholderText('ConfirmPassword'), '!Qwer2333');
+    userEvent.click(getByRole('button', { name: 'Submit' }));
     await waitFor(() => {
       expect(getByText('accessGranted')).toBeInTheDocument();
     });
-  });
-  it('redirects user if already authenticated', () => {
-    (useAuth as Mock).mockReturnValueOnce({
-      user: { email: 'test@example.com' },
-      loading: false,
-    });
-
-    render(<LoginForm />);
-    expect(mockNavigate).toHaveBeenCalledWith('/');
   });
 
   it('displays loading component when loading is true', () => {
     (useAuth as Mock).mockReturnValueOnce({ user: null, loading: true });
 
-    const { getByRole } = render(<LoginForm />);
+    const { getByRole } = render(<SignUpForm />);
 
     expect(getByRole('status')).toBeInTheDocument();
+  });
+
+  it('redirects user if already authenticated', () => {
+    (useAuth as Mock).mockReturnValueOnce({
+      user: { displayName: 'JohnDoe' },
+      loading: false,
+    });
+
+    render(<SignUpForm />);
+    expect(mockNavigate).toHaveBeenCalledWith('/');
   });
 });
